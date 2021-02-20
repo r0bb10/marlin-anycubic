08c5344722264e6697a3d90378fd0919b0d3b6d2
r0bb10 <anile.roberto@gmail.com>
2021-02-20 17:53:01 +0100

    Revert extruder steps

--- Configuration.h
+++ Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 450.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 402.9 }
 
 /**
  * Default Max Feed Rate (mm/s)
